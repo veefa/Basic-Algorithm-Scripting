d6b219d97fe6a1eb73d5b9003d515d42344b3d80
vefa <44091688+veefa@users.noreply.github.com>
2021-08-20 12:02:36 +0100

    Create finder.js

---
+++ finder.js
@@ -0,0 +1,17 @@
+//Problem
+/*
+ create a function that loops through an array (arr) and returns the first element in it that passes a 'truth test'. 
+ This means that given an element x, the 'truth test' is passed if func(x) is true. 
+ If no element passes the test, return undefined.
+*/
+function findElement(arr, func) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+
+      if (func(num) == func(arr[i])) {
+        return num;
+      }
+    }
+  return undefined;
+}
+console.log(findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0;}));
