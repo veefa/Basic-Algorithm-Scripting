9334722bb3847853ea6c5bdbb4386a9841eed1fe
vefa <44091688+veefa@users.noreply.github.com>
2021-08-20 11:55:24 +0100

    Create boo_who.js

---
+++ boo_who.js
@@ -0,0 +1,7 @@
+//Checking if a value (bool) is classified as a boolean primitive, simply if it's true or false. Return true or false.
+
+function booWho(bool) {
+ return bool === true || bool === false ? true : false;
+}
+
+booWho(null);
