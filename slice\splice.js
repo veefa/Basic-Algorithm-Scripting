6042ba637c7af54adca488eac84cb3d56fe08df9
vefa <44091688+veefa@users.noreply.github.com>
2021-08-22 15:19:45 +0100

    Update slice\splice.js

--- slice\splice.js
+++ slice\splice.js
@@ -6,6 +6,16 @@ Begin inserting elements at index n of the second array.
 
 Return the resulting array. The input arrays should remain the same after the function runs.
 */
+//Solotion 1 
+///
+// Using "..." operatore to insert elements
+///
+//
+/*Algorithm
+  1 - make a copy of arr2 with slice() (where we make changes)
+  2 - insert arr1 elements starting from index n and delete 0 with splice()
+  3 - return the arrays combined
+*/
 function frankenSplice(arr1, arr2, n) {
   let newArr = arr2.slice();
   newArr.splice(n, 0, ...arr1);
@@ -14,3 +24,25 @@ function frankenSplice(arr1, arr2, n) {
 
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
 // return [4, 1, 2, 3, 5, 6]length :6
+//Solotion 2
+///
+// Using for loop
+///
+//
+/*Algorithm
+  1 - make a copy of arr2 (where we make changes)
+  2 - loop through arr1 element and insert them in the copy array starting from index n
+  3 - increase n by 1 each the loop runs
+  4 - return the arrays combined
+*/
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.splice(n, 0, arr1[i]);
+    n++
+  }
+  return newArr;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+// return [4, 1, 2, 3, 5, 6]
